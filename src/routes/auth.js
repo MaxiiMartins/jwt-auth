51412ba51b71d3fda495780beacb6a3605c59265
Maximiliano <maximartins45@gmail.com>
2022-10-25 22:39:40 -0300

    Update auth.js

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,17 +1,10 @@
 const {Router} = require("express")
-const { Users } = require('../db');
-const bcrypt = require('bcrypt');
+const {login,register} = require("../controllers/auth.controllers")
 
 const router = Router()
 
 
-router.post("/login",async(req,res)=>{
-    const {email,password} = req.body
-    const isEmailExist = await Users.findOne({where:{email}})
-    console.log(await bcrypt.compare(password, isEmailExist.password))
-    if(!isEmailExist) res.status(404).send(`Email no registrado`)
-    else if(!await bcrypt.compare(password, isEmailExist.password)) res.status(404).send(`Password incorrecto`)
-    else res.status(200).send(`Bienvenido ${isEmailExist.fullname}`)
-})
+router.post("/login",login)
+
 
 module.exports = router
