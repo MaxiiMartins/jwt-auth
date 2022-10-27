0a6f8aeb3e6dd5db71e2f1bb8215f2e2cc34ecff
Maximiliano <maximartins45@gmail.com>
2022-10-26 23:17:15 -0300

    Update auth.controllers.js

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -3,48 +3,38 @@ const { Users } = require('../db');
 const jwt = require("jsonwebtoken");
 const JWT_SECRET = process.env.JWT_SECRET
 
-const login = async(req,res)=>{
-    console.log(JWT_SECRET || "no hay")
-    const {email,password} = req.body
-    console.log(req.body)
-    const hola = jwt.sign({
-        email,
-        password
-    },JWT_SECRET,{expiresIn:"1000"})
-    console.log(hola)
-    console.log(jwt.verify(hola,JWT_SECRET))
-    
+const login = async (req, res) => {
+    const { email, password } = req.body
     try {
-        const isEmailExist = await Users.findOne({where:{email}})
-        if(!isEmailExist) res.status(404).send(`Email no registrado`)
-        else if(!await bcrypt.compare(password, isEmailExist.password)) res.status(404).send(`Password incorrecto`)
-        else res.status(200).send(`Bienvenido ${isEmailExist.fullname}`)
+        const isEmailExist = await Users.findOne({ where: { email } })
+        if (!isEmailExist) res.status(404).send(`Email no registrado`)
+        else if (!await bcrypt.compare(password, isEmailExist.password)) res.status(404).send(`Password incorrecto`)
+        else {
+            const token = jwt.sign({
+                user_id: isEmailExist.id,
+                fullname: isEmailExist.fullname,
+                email: isEmailExist.email
+                },JWT_SECRET,
+                { expiresIn: "1000" })
+            
+            res.cookie("ftoken", token,{
+                httpOnly: true,
+                secure: process.env.NODE_ENV === "production",
+                sameSite: "none",
+                maxAge: 2000,
+              })
+            res.status(200).json({
+                response:isEmailExist
+            })
+        }
+
     } catch (error) {
         res.status(404).json({
-            error:error.message
+            error: error.message
         })
     }
 }
 
-// router.post("/register",async(req,res)=>{
-//     const {fullname,email,password} = req.body
-//     const isEmailExist = await Users.findOne({where:{email}})
-//     if(isEmailExist) return res.status(404).send(`Email ya existe`)
-//     const hashedPassword = await bcrypt.hash(password, 10)
-//     const newUser = await Users.create({
-//         fullname,
-//         email,
-//         password:hashedPassword
-//     })
-//     res.status(200).json({
-//         message:`Usuario registrado correctamente`,
-//         user:newUser
-//     })
-
-// })
-
-const register = "ok"
 module.exports = {
-    login,
-    register
+    loginControllers
 }
